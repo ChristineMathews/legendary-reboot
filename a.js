2ae888bd4183f96e521e10ddff883c6d2b3e2335
Christine Mathews <34720216+ChristineMathews@users.noreply.github.com>
2023-04-25 14:50:28 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Component1 from './Component1';
 import Component2 from './Component2';
 
-const App = () => {
+const ParentComponent = () => {
   return (
-    <Router>
-      <Switch>
-        <Route
-          path="/some-path"
-          render={() => (
-            <div>
-              <Component1 />
-              <Component2 />
-            </div>
-          )}
-        />
-      </Switch>
-    </Router>
+    <div>
+      <Component1 />
+      <Component2 />
+    </div>
   );
 };
 
-export default App;
+export default ParentComponent;
